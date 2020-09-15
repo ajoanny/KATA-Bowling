98af2d363e0243f3b71801c034970120d9ec2a30
Laurent <laurent.lucas@pix.fr>
2020-09-15 17:40:11 +0200

    go alex

--- app/project.js
+++ app/project.js
@@ -2,10 +2,15 @@ class Project {
 
     calculateScore(list) {
         let result = 0;
-        list.forEach(score => {
+        let resultTmp = 0
+        list.forEach((score, index) => {
+
             if(score === 10 ){
                 result += 1
+            } else if((score + list[index + 1]) === 10) {
+                result =+ list[index + 2]
             }
+
             result += score
         })
         return result;
